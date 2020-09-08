08468d669815cff3c13eb304b3893a39adcf746b
RGalways17 <66765133+RGalways17@users.noreply.github.com>
2020-09-08 23:50:55 +0800

    Update linked_list.c

--- lab1/linked_list.c
+++ lab1/linked_list.c
@@ -57,14 +57,29 @@ void linked_list_append(node *head, int val){
 /* your implementation goes here */
 
 void linked_list_insert(node *head, int val, int index){
-    node* temp = head;
-    node *new_node = (node*)malloc(sizeof(node));
-    new_node->value = val;
-    new_node->next = NULL;
-    for(int i = 0; i < index;i++){
-        temp=temp->next;
-    }
-    new_node->next = temp->next;
-    temp->next = new_node;
-    head->count++;
+
+}
+
+void linked_list_delete(node *head, int index){
+
+}
+
+void linked_list_remove(node *head, int val){
+
+}
+
+void linked_list_remove_all(node *head, int val){
+
+}
+
+int linked_list_get(node *head, int index){
+    return 0;
+}
+
+int linked_list_search(node *head, int val){
+    return 0;
+}
+
+node *linked_list_search_all(node *head, int val){
+    return linked_list_init();
 }
